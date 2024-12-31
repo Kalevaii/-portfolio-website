c9cadc51397eb77b82422974213052df8b680155
Pranaya Poudel <35399048+Kalevaii@users.noreply.github.com>
2024-12-31 15:15:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Smooth Scroll for Navigation Links
 document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent default anchor behavior
+        // Exclude links that point to external files (like the CV PDF) from smooth scrolling
+        if (this.getAttribute('href').endsWith('.pdf')) {
+            return; // Allow the default behavior for PDF links
+        }
+
+        e.preventDefault(); // Prevent default anchor behavior for non-PDF links
 
         const targetId = this.getAttribute('href'); // Get the target section ID from href
         const targetElement = document.querySelector(targetId); // Find the target element
